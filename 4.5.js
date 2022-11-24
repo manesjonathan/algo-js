30e6dcd038c24ccfc14de8f4dee891a46a392188
Jonathan Manes <manesjona@gmail.com>
2022-11-24 16:12:18 +0100

    Exercise 4.5

---
+++ 4.5.js
@@ -0,0 +1,12 @@
+function calcDistance(x1, y1, x2, y2) {
+
+    let y = x2 - x1;
+    let x = y2 - y1;
+
+    return Math.sqrt(x * x + y * y).toFixed(2);
+}
+
+console.log(calcDistance(1, 1, 2, 2));
+console.log(calcDistance(1, 1, 3, 1));
+console.log(calcDistance(4, 1, 1, 1));
+console.log(calcDistance(-2, 2, 2, -2));
