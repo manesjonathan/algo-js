0a880ef97cec4d0df7d16c35766b074d6dfcd071
Jonathan Manes <manesjona@gmail.com>
2022-11-24 14:23:00 +0100

    Exercise 4.2

---
+++ 4.2.js
@@ -0,0 +1,13 @@
+/*
+This function generate a random Integer between a given range.
+This example use a range from 1 to 10.
+*/
+function rand10(value1, value2) {
+
+    min = Math.ceil(value1);
+    max = Math.floor(value2);
+
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+console.log(rand10(1, 10));
