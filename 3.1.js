c258fc747a14f39bf90a14b3e875bb5ca6769a3f
Jonathan Manes <manesjona@gmail.com>
2022-11-24 13:08:48 +0100

    Exercise 3.1

---
+++ 3.1.js
@@ -0,0 +1,16 @@
+let numberArrayOne = [1, 2, 3, 4, 5];
+let numberArrayTwo = [100, 101, 102];
+let sumArrayOne = null;
+let sumArrayTwo = null;
+
+for (number of numberArrayOne){
+    sumArrayOne = sumArrayOne + number;
+}
+
+console.log(sumArrayOne);
+
+
+for (number of numberArrayTwo){
+    sumArrayTwo = sumArrayTwo + number;
+}
+console.log(sumArrayTwo);
