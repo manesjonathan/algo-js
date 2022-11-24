f62e488641c52f1304887bdccfbbcd35c2f85948
Jonathan Manes <manesjona@gmail.com>
2022-11-24 11:20:12 +0100

    Exercise 2.4

---
+++ 2.4.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 100; i += 1) {
+    if (!(i % 2 > 0)) {
+
+        console.log(i / 2);
+    } else if (i % 2 > 0) {
+        {
+            console.log(i * 3);
+        }
+    }
+}
