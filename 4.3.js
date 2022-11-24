8c2a2b4e8caf2da93360b302ca01a223fd65f13d
Jonathan Manes <manesjona@gmail.com>
2022-11-24 15:04:59 +0100

    Exercise 4.4

---
+++ 4.3.js
@@ -0,0 +1,37 @@
+/*
+This function generate randoms Integer between a given range.
+This example use a range from 1 to 10 and ask user for how many numbers the function should return.
+*/
+function multiRand(n) {
+    let array = [];
+    for (let i = 1; i <= n; i += 1) {
+        array.push(rand10(1, 10));
+    }
+    console.log(array);
+}
+
+function rand10(value1, value2) {
+
+    min = Math.ceil(value1);
+    max = Math.floor(value2);
+
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function exec() {
+    let input = null;
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+
+    rl.question("Please enter a number:\n", function (number) {
+        let input = number;
+        multiRand(input);
+        rl.close()
+    });
+}
+
+exec();
