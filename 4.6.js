664102d7e470adf3a66a33e8aa1c25dd8170240a
Jonathan Manes <manesjona@gmail.com>
2022-11-24 16:20:21 +0100

    Exercise 4.6

---
+++ 4.6.js
@@ -0,0 +1,14 @@
+/*
+This function take a number and factorize it .
+*/
+
+function factorize(num) {
+    if (num < 0) 
+          return -1;
+    else if (num == 0) 
+        return 1;
+    else {
+        return (num * factorize(num - 1));
+    }
+  }
+  console.log(factorize(3));
