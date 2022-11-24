a9758562fb2412352f59743de8df8e22a9c9a9a5
Jonathan Manes <manesjona@gmail.com>
2022-11-24 11:15:53 +0100

    Correction 2.3

--- 2.3.js
+++ 2.3.js
@@ -1,9 +1,17 @@
 let i = 1;
-while (i <= 100){
-    console.log(i);
+while (i <= 100) {
+
+    if (!(i % 2 > 0)) {
+
+        console.log(i);
+    }
     i += 1;
 }
 
+
 for (let i = 1; i <= 50; i += 1) {
-    console.log(i);
-  }
+    if (!(i % 2 > 0)) {
+
+        console.log(i);
+    }
+}
